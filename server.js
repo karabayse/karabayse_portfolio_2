d8e1c1dc075f100626150567e42511f87f828a44
Kara Bayse <karabayse@gmail.com>
2017-09-27 14:14:21 -0500

    server up

---
+++ server.js
@@ -0,0 +1,20 @@
+// requires
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var port = process.env.Port || 2005;
+
+// uses
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+  console.log('base url hit');
+  res.sendFile(path.resolve('public/views/index.html'));
+});
+
+app.listen(port, function(){
+  console.log('listening on 2005 ->', port);
+});
